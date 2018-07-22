404f80d40058f4c203c2efdd8a4484206888baf1
lizhanyu <lizhanyu@zhangyue.com>
2018-07-22 13:55:59 +0800

    连接redis test

--- lua/redis_test.lua
+++ lua/redis_test.lua
@@ -8,4 +8,17 @@ if not ok then
         ngx.say("failed to connect: ", err)
         return
 end
-ngx.say("set result: ", ok)
+ngx.say("set result: ", "connect success")
+local res, err = red:get("book")
+
+if not res then
+        ngx.say("failed to get book: ", err)
+        return
+end
+ngx.say("get book is: ", res)
+if res == ngx.null then
+        ngx.say("book not found.")
+        return
+end
+
+ngx.say("book: ", res)
