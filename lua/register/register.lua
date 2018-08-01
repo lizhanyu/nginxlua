f49e2bce63cd1044bbfba080f7f87bdfb3e20584
lizhanyu <lizhanyu@zhangyue.com>
2018-08-01 19:00:15 +0800

    111

--- lua/register/register.lua
+++ lua/register/register.lua
@@ -5,17 +5,12 @@ local username = uri_args.username
 local password = uri_args.password
 local info 	   = uri_args.info 
 
---[[for k, v in pairs(uri_args) do  
-    if k == "username" then
-    	username  = v
-    end
-    if k ==   
-end  --]]
+
  ngx.say("username : ", username, " , password : ", password, " , info : ", info) 
  require "lua/register/register_db"
  register_db.init()
 
- register_db:insertUser(username,password,info)
+ register_db.insertUser(username,password,info)
 
 
 
