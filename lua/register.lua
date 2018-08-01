c02dcfdf5b27981fe24fcf5487d5c1656b0d21e1
lizhanyu <lizhanyu@zhangyue.com>
2018-08-01 15:55:07 +0800

    测试

--- lua/register.lua
+++ lua/register.lua
@@ -1,3 +1,11 @@
 local var = ngx.var  
-ngx.say("ngx.var.a : ", var.name, "<br/>")  
-ngx.say("ngx.var.b : ", var.password, "<br/>")  
+ngx.say("uri args begin", "<br/>")  
+local uri_args = ngx.req.get_uri_args()  
+for k, v in pairs(uri_args) do  
+    if type(v) == "table" then  
+        ngx.say(k, " : ", table.concat(v, ", "), "<br/>")  
+    else  
+        ngx.say(k, ": ", v, "<br/>")  
+    end  
+end  
+ngx.say("uri args end", "<br/>")  
