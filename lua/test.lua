55847be43d4966b6c60aebbf8489a8f1e3322c51
lizhanyu <lizhanyu@zhangyue.com>
2018-08-07 09:51:28 +0800

    测试的test文件

--- lua/test.lua
+++ lua/test.lua
@@ -1,5 +1,14 @@
-ngx.say("hello lizy");   
+ngx.say("Hi Gary");   
 
 local cjson = require("cjson")  
 local value = cjson.encode(ngx.shared.url_limit)
 ngx.say(value)
+
+
+--[[
+把圈子变小，把语言变干净，把成绩往上提，把故事往心里收一收，现在想要的以后你都会有。
+总会有不期而遇的温暖，也会存在生生不息的希望。虽任重而道远，只要方向正确，不管多么崎岖不平，都比站在原地更接近终点。
+
+
+
+--]]
